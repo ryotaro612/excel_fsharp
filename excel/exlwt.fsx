eaedbdc8974b4f9e4478be3ca1769083c7dd23e4
Ryotaro NAKAMURA <r.nakamura1989@gmail.com>
2016-04-27 08:39:44 +0900

    append

---
+++ excel/exlwt.fsx
@@ -0,0 +1,11 @@
+﻿
+open System
+
+let rec getLines lines :String List  =
+    let a = Console.ReadLine()
+    sprintf "%s" a
+    getLines (List.append lines [a])
+
+let a: String list = getLines []
+
+a |> List.iter (fun x -> printf "%s " x)
